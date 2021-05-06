cb14a85fafda39b5fbb49688b2fdf696420b3d58
dasith <dasiththisaru@gmail.com>
2021-05-06 16:18:06 +0530

    Create enqueue

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,7 +1,20 @@
 public class Queue {
-
+    
+    
+    private int[] numbers;
+    
     public void enQueue(int number){
-
+        if(numbers==null){
+            numbers = new int[1];
+            numbers [0] = number;
+        }else{
+            int[] temp = new int[numbers.length+1];
+            for (int i = 0; i < numbers.length; i++) {
+                temp[i] = numbers[i];
+            }
+            temp[temp.length-1]=number;
+            numbers = temp;
+        }
     }
     public void dQueue(){
 
