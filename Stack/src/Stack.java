a1ae798b54d1abed0ab46c096afb1f0e58114105
dasith <dasiththisaru@gmail.com>
2021-05-06 15:32:36 +0530

    recorrection1

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,15 +1,28 @@
 public class Stack {
 
-
+    private  int[] numbers;
 
     public void push(int number){
+        if(numbers==null){
+           numbers = new int[1];
+           numbers[0] = number;
+        }else {
+            int[] temp = new int[numbers.length+1];
+            for (int i = 0; i < numbers.length ; i++) {
+                temp[i] = numbers[i];
+            }
+            temp[temp.length-1]=number;
+            numbers = temp;
+        }
+
+
 
     }
     public void pop(){
 
     }
     public boolean empty(){
-        return  false;
+        return numbers==null;
     }
     public int peek(){
         return 0;
--- Stack/src/Stacktest.java
+++ Stack/src/Stacktest.java
@@ -2,6 +2,7 @@ public class Stacktest {
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.empty();
+        stack.push(2);
 
     }
 }
