66eb47da4077c7c06b189423bd30de7377391830
dasith <dasiththisaru@gmail.com>
2021-05-06 10:52:31 +0530

    create method

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,2 +1,26 @@
 public class Stack {
+
+
+    public void push(int number){
+        System.out.println();
+    }
+    public void pop(){
+
+    }
+    public boolean empty(){
+
+    }
+    public int peek(){
+
+    }
+    public void clear(){
+
+    }
+    public void print(){
+
+    }
+    public  boolean contains(int number){
+
+    }
+
 }
