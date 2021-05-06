bb76768e7f618c747a04c6ce91b3a799bcc09c11
dasith <dasiththisaru@gmail.com>
2021-05-06 16:11:32 +0530

    create methd and class

---
+++ Queue/src/Queue.java
@@ -0,0 +1,29 @@
+public class Queue {
+
+    public void enQueue(int number){
+
+    }
+    public void dQueue(){
+
+    }
+    public boolean empty(){
+        return false;
+    }
+    public int peek(){
+        return 0;
+    }
+    public void clear(){
+
+    }
+    public void print(){
+
+    }
+    public boolean contains(int number){
+        return false;
+    }
+    public int size(){
+        return 0;
+    }
+
+
+}
