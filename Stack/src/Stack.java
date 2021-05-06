0fffc6a54ca38edee3385be11a860cec99f70f72
dasith <dasiththisaru@gmail.com>
2021-05-06 15:12:49 +0530

    recorrection 1

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -1,43 +1,27 @@
 public class Stack {
 
-    int num[] = new int[100];
 
 
     public void push(int number){
-        for (int i = 0; i < num.length; i++) {
-            num[i] = number;
-        }
 
     }
     public void pop(){
-        for (int i = 0; i < num.length; i++) {
-        }
 
     }
     public boolean empty(){
-
+        return  false;
     }
     public int peek(){
-        //Dont know
+        return 0;
     }
     public void clear(){
-        num = null;
+
     }
     public void print(){
-        for (int i = 0; i < num.length; i++) {
-            System.out.println(num[i]);
-        }
 
     }
-    public  boolean contains(int number){
-        for (int i = 0; i < num.length; i++) {
-            if (number != num[i]) {
-                 return false;
-            }else{
-                return  true;
-            }
-        }
-
+    public boolean contains(int number){
+        return false;
     }
 
 }
---
+++ Stack/src/Stacktest.java
@@ -0,0 +1,7 @@
+public class Stacktest {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.empty();
+
+    }
+}
